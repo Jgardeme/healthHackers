ab1a5cf65d27d0bbc5bdd1b069d88b4a75c7c606
Sowrov Doha <sowrov.doha@gmail.com>
2018-12-15 21:33:35 +0200

    start screen

---
+++ Assets/StreamVideoWithVideoPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class StreamVideoWithVideoPlayer : MonoBehaviour
+{
+
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayVideo());
+    }
+
+    IEnumerator PlayVideo()
+    {
+        videoPlayer.Prepare();
+        WaitForSeconds WaitForSeconds = new WaitForSeconds(1);
+        while(!videoPlayer.isPrepared)
+        {
+            yield return WaitForSeconds;
+            break;
+        }
+        rawImage.texture = videoPlayer.texture;
+        audioSource.Play();
+    }
+
+ /*   // Update is called once per frame
+    void Update()
+    {
+        
+    }^*/
+}
