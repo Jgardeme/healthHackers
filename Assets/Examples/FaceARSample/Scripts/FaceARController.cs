cd511e9be0b9bdfc1ae7fd3a58c0135b2c9bde60
Jgardeme <joni.gardemeister@aalto.fi>
2018-12-16 00:40:08 +0200

    Working version 2.1

--- Assets/Examples/FaceARSample/Scripts/FaceARController.cs
+++ Assets/Examples/FaceARSample/Scripts/FaceARController.cs
@@ -36,15 +36,6 @@
             }
         }
 
-        private void RequestToServer()
-        {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("http://api.openweathermap.org/data/2.5/weather?id={0}&APPID={1}", 11, "API-key"));
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            StreamReader reader = new StreamReader(response.GetResponseStream());
-            string jsonResponse = reader.ReadToEnd();
-            Debug.Log(jsonResponse);
-        }
-
         public void startVid()
         {
             //RequestToServer();
@@ -71,81 +62,7 @@
         {
             Debug.Log("START");
             Handheld.PlayFullScreenMovie(file, Color.black, FullScreenMovieControlMode.Full);
-            /*GameObject camera = GameObject.Find("PreviewCamera");
-
-            //GameObject videoCamera = GameObject.Find("VideoCamera");
-
-            //Destroy(GameObject.Find("HuaweiAR Device"));//.SetActive(false);
-
-
-            //imagePlane = GameObject.Find("ImagePlane");
-
-            var videoPlayer = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
-            videoPlayer.playOnAwake = false;
-            videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
-            //videoPlayer.targetCameraAlpha = 0.5F;
-            videoPlayer.url = file;
-            //videoPlayer.playbackSpeed = videoPlayer.playbackSpeed / 2.0F;
-            //videoPlayer.frame = 100;
-            videoPlayer.isLooping = true;
-
-
-            videoPlayer.loopPointReached += EndReached;
-            videoPlayer.Prepare();
-
-            while (!videoPlayer.isPrepared)
-            {
-                Debug.Log("Preparing video!");
-                //yield return new WaitForSeconds(1);
-            }
-
-            Debug.Log("DONE");
-            imagePlane.texture = videoPlayer.texture;
-
-            videoPlayer.Play();
-
-            while (videoPlayer.isPlaying)
-            {
-                Debug.Log("Playing video at " + Mathf.FloorToInt((float)videoPlayer.time));
-                yield return null;
-            }
-
-            Debug.Log("DONE PLAYING");
-            */
-
-            //camera.SetActive(false);
-            //videoCamera.SetActive(true);
-
-
-            /*MonoBehaviour[] components = facePrefabs.GetComponents<MonoBehaviour>();
-            foreach(MonoBehaviour c in components)
-            {
-                c.enabled = false;
-                Debug.Log(c.isActiveAndEnabled);
-            }*/
-
-            //Destroy(camera);
-            //GameObject videoCamera = GameObject.Find("")
-
-
-            //var videoPlayer = videoCamera.AddComponent<UnityEngine.Video.VideoPlayer>();
-            /* var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
-            videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
-            //videoPlayer.targetCameraAlpha = 0.5F;
-            videoPlayer.url = file;
-            //videoPlayer.playbackSpeed = videoPlayer.playbackSpeed / 2.0F;
-            //videoPlayer.frame = 100;
-            videoPlayer.isLooping = true;
-            videoPlayer.loopPointReached += EndReached;
-            videoPlayer.Play(); */
         }
-
-        /*void EndReached(UnityEngine.Video.VideoPlayer vp)
-        {
-            Debug.Log("sdfsdfsdf");
-            //Destroy(vp);
-            //vp.Stop();
-        }*/
 
         IEnumerator setRequest(string videoPath)
         {
@@ -180,7 +97,7 @@
                 else
                 {
                     Debug.Log("Form upload complete!");
-                    Debug.Log(www.downloadHandler.text);                     byte[] results = www.downloadHandler.data;                     Debug.Log(results.Length);                     Debug.Log(results);                     string path = "/storage/emulated/0/DCIM/VideoRecorders/";                     File.WriteAllBytes(path + fileName + "_output.mp4", results);                     while (!System.IO.File.Exists(path + fileName + "_output.mp4"))                     {                         yield return new WaitForSeconds(1);                     }                     ShowTheVideo(path + fileName + "_output.mp4");                     /*foreach (KeyValuePair<string, string> entry in www.GetResponseHeaders())                     {                         Debug.Log(entry.Value + "=" + entry.Key);                     }*/
+                    Debug.Log(www.downloadHandler.text);                     byte[] results = www.downloadHandler.data;                     Debug.Log(results.Length);                     Debug.Log(results);                     string path = "/storage/emulated/0/DCIM/VideoRecorders/";                     File.WriteAllBytes(path + fileName + "_output.mp4", results);                     while (!System.IO.File.Exists(path + fileName + "_output.mp4"))                     {                         yield return new WaitForSeconds(1);                     }                     ShowTheVideo(path + fileName + "_output.mp4");
                 }
             }
         }
